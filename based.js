534fb494628a45acc7334fb8b4fee4638cf03bf6
flexsyyy <aqeeb.talib@gmail.com>
2022-12-06 10:07:00 +0000

    Create 4 files

---
+++ based.js
@@ -0,0 +1,24 @@
+
+//library of javascript
+
+
+//const a = {
+  //  name:"mirza",
+   // surname: "TF",
+   ///// sex:"Male",
+    
+//};
+
+//console.log(a.sex);
+
+// array
+
+//const data type is important
+//try {
+   // console.log(b.urmom);
+    
+
+//  }  catch(err) {
+ //   console.log("urmom is gay");
+//}
+
